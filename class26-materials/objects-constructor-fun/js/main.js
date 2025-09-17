420cc5d30b19375b59f89256c168f0e08df0264b
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-17 10:13:33 +0000

    Create pizza constructor

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizza(crust, size, slices, price) {
+    this.crust = crust;
+    this.size = size;
+    this.slices = slices;
+    this.price = price;
+
+    this.eatSlices = () => {
+        console.log(`You ate a slice of ${this.size} ${this.crust} crust pizza.`);
+        this.slices--;
+    }
+
+    this.getPrice = () => {
+        return this.price;
+    }
+
+    this.getDescription = () => {
+        return `This is a ${this.size} ${this.crust} crust pizza with ${this.slices} slices left.`;
+    }
+}
