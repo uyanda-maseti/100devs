04486e0413bd240ce94314723705f3fbc2a13ed7
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-17 09:12:39 +0000

    Create a pizza object using literal notation

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,24 @@
 //Create a dog object that has four properties and three methods
+let  pizza = {};
+
+pizza.numberOfSlices = 8;
+pizza.layers = 3;
+pizza.size = "large";
+pizza.crust = "stuffed";
+pizza.extraCheese = true;
+
+pizza.eat = function() {
+    return "Nom nom nom!";
+};
+
+pizza.delivery = function() {
+    return "Your pizza is on its way!";
+};
+
+pizza.describe = function() {
+    return `This pizza is a ${this.size} size with ${this.crust} crust and has ${this.numberOfSlices} slices.`;
+};
+
+console.log(pizza.eat());
+console.log(pizza.delivery());
+console.log(pizza.describe());
