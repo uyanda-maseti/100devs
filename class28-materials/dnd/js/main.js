1d88274ac009568a554e6ea3b1e3a8feba02c267
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-22 09:30:17 +0000

    DND

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -3,12 +3,12 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+  const url = `https://www.dnd5eapi.co/api/2014/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data.subclasses)
       })
       .catch(err => {
           console.log(`error ${err}`)
