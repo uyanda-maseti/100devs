79cb0bf1c7b95e46d6c73486b5da05c92275f25c
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-23 06:58:12 +0000

    add class30 materials

--- class29-materials/boulder-badge/js/main.js
+++ class29-materials/boulder-badge/js/main.js
@@ -1,6 +1,10 @@
 // Variables
 //You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
-
+const bulbasaurCandy = 16 - 5;
+const caterpieCandy = 7 - 1;
+const weedleCandy = 7 - 1;
+const totalCandies = bulbasaurCandy + caterpieCandy + weedleCandy;
+console.log(totalCandies);
 //Conditionals And Functions
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
 
