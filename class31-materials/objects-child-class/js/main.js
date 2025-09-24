8eee765117622b3322fe3cb94854f9d41985a8b4
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-24 09:22:54 +0000

    Make a child

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,27 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
-
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this.breed = breed
+    } 
+}
+class Cat extends Animal{
+    constructor(name,color){
+        super(name)
+        this.color = color
+    }
+    speak(){
+        console.log(`${this.name} meows`)
+    }
+}
+let stanley = new Cat('Stanley', 'Black')
+console.log(stanley)
+let simba = new Dog('Simba', 'Sheperd')
