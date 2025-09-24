76ae66135efc37827aa21229c7a15e43f5a885d8
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-24 07:15:59 +0000

    object-espresso-machine

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,26 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine {
+    constructor(color, brand, model, waterLevel){
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+        this.waterLevel = waterLevel;
+
+    }
+    brewEspresso(){
+        if(this.waterLevel > 0){
+            console.log("Brewing your espreesso...");
+            this.waterLevel--;
+        }
+    }
+    refillWater(){
+        if(this.waterLevel < 10){
+            this.waterLevel++;
+            console.log("Refilling water...");
+            console.log(`Water level: ${this.waterLevel}`);
+        }else{
+            console.log("Water tank is full!" + this.waterLevel);
+        }
+    }
+    
+} 
