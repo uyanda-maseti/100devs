362185df387b8f70e0c25d9d40746f76e9ef2642
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-09-30 13:27:32 +0000

    Unlock Full-stack special skill

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -43,18 +43,18 @@
 // houseTwo(houseThree)
 
 //Code 04
-// function houseOne(){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 1')
-//         setTimeout(() => {
-//             console.log('Paper delivered to house 2')
-//             setTimeout(() => {
-//                 console.log('Paper delivered to house 3')
-//             }, 3000)
-//         }, 4000)
-//     }, 5000)
-// }
-// houseOne()
+function houseOne(){
+    setTimeout(() => {
+        console.log('Paper delivered to house 1')
+        setTimeout(() => {
+            console.log('Paper delivered to house 2')
+            setTimeout(() => {
+                console.log('Paper delivered to house 3')
+            }, 3000)
+        }, 4000)
+    }, 5000)
+}
+houseOne()
 
 //Code 05
 // const promise = new Promise((resolve, reject) => {
