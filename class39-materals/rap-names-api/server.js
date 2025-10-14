00f9ff3e840aec40a8fca42f9a9590cc164dfa7c
uyanda-maseti <128975995+uyanda-maseti@users.noreply.github.com>
2025-10-14 06:36:30 +0000

    Add more rappers

--- class39-materals/rap-names-api/server.js
+++ class39-materals/rap-names-api/server.js
@@ -3,11 +3,22 @@ const app = express()
 const PORT = 8000
 
 
-const savage = {
-    'age': 29,
-    'birthName': 'Shéyaa Bin Abraham-Joseph',
-    'birthLocation': 'London, England',
-}
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, Illinois',
+    },
+    'Nasty C': {
+        'age': 26,
+        'birthName': 'Nsikayesizwe David Junior Ngcobo',
+        'birthLocation': 'Durban, South Africa',
+} 
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
